04d30e4bdc54cc37bea480b5fd5928eb1a02e9ff
Brent Vollebregt <volleys2468@gmail.com>
2017-11-22 12:34:54 +1300

    Article CSS, syntax highlighting (pygments), small modifications

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, render_template_string, send_from_directory, abort
+from flask import Flask, render_template , url_for, render_template_string, send_from_directory, abort
 
 import data_managers
 
