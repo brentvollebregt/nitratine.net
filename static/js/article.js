425cfd69bd58c5ae7782d3e55397352f0b07c588
Brent Vollebregt <volleys2468@gmail.com>
2018-01-20 17:15:48 +1300

    Right sidebar appears on smaller devices and added space for an ad in right sidebar.

--- static/js/article.js
+++ static/js/article.js
@@ -4,12 +4,18 @@ sheet = style.sheet;
 sheet.addRule('h1:before', 'background-image: url(/non-static' + window.location.pathname + '/icon.png)');
 
 function rightSidebarCalculations() {
-    // TODO Make a right sidebar
-    // Check that the nav bar is showing
-    // Make sure there is div.article_content for main content
-    // Check that there is reasonable space for the section (test this to find thee width)
-    // Calculate and set new size of content
-    // Make right sidebar visible (300px wide; same as nav)
+    if (window.innerWidth >= 1280) {
+        // 280 nav, min 700 content, 300 right nav
+        if (document.getElementsByClassName('article_content').length === 1) {
+            var new_width = window.innerWidth - 580;
+            document.getElementsByClassName('article_content')[0].style.maxWidth = new_width + 'px';
+            document.getElementById('right_sidebar').style.display = 'block';
+            document.getElementById('right_sidebar').style.marginLeft = (window.innerWidth - 300) + 'px';
+        }
+    } else {
+        document.getElementsByClassName('article_content')[0].style.maxWidth = '1200px';
+        document.getElementById('right_sidebar').style.display = 'none';
+    }
 }
 
 window.addEventListener('load', function() {
