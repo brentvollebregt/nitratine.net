ac84fbaceaef2f417449c3b03d7673f8fdcd881d
Brent Vollebregt <volleys2468@gmail.com>
2018-01-22 22:59:00 +1300

    Removed static ad sizing and keep .article_content max-width of 1200px.

--- static/js/article.js
+++ static/js/article.js
@@ -4,16 +4,24 @@ sheet = style.sheet;
 sheet.addRule('h1:before', 'background-image: url(/non-static' + window.location.pathname + '/icon.png)');
 
 function rightSidebarCalculations() {
-    if (window.innerWidth >= 1295 && enable_right_sidebar) {
+    if (window.innerWidth >= 1295 && enable_right_sidebar && window.innerWidth < 1795) {
         // 280 nav, min 700 content, 300 right nav + 15 margin
         if (document.getElementsByClassName('article_content').length === 1) {
             var new_width = window.innerWidth - 595;
-            document.getElementsByClassName('article_content')[0].style.maxWidth = new_width + 'px';
+            document.getElementsByClassName('article_content')[0].style.width = new_width + 'px';
             document.getElementById('right_sidebar').style.display = 'flex';
             document.getElementById('right_sidebar').style.marginLeft = (window.innerWidth - 315) + 'px';
         }
+    } else if (window.innerWidth > 1795) {
+        // 280 nav, max 1200 content, 300 right nav + 15 margin
+        if (document.getElementsByClassName('article_content').length === 1) {
+            document.getElementsByClassName('article_content')[0].style.width = '1200px';
+            document.getElementById('right_sidebar').style.display = 'flex';
+            document.getElementById('right_sidebar').style.marginLeft = '1480px';
+        }
     } else {
         document.getElementsByClassName('article_content')[0].style.maxWidth = '1200px';
+        document.getElementsByClassName('article_content')[0].style.width = '';
         document.getElementById('right_sidebar').style.display = 'none';
     }
 }
