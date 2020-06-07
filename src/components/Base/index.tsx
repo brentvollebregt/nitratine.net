d2d4786e67ddb65441f863b9ec9cec634b075562
Brent Vollebregt <brent.vollebregt@gmail.com>
2020-06-07 12:42:49 +1200

    Setup workflow to build site

--- src/components/Base/index.tsx
+++ src/components/Base/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Navigation from "./Navigation";
-import "./all.scss";
+import "./Base.scss";
 
 const Base = ({ children }) => {
   return (
     <>
+      <Helmet>
+        {/* Temporary */}
+        <meta name="robots" content="noindex" />
+      </Helmet>
+
       <Navigation />
       <main role="main" className="container my-4">
         {children}
