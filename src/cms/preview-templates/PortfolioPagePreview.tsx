bc0297e89f3591635a264e40aba06b8f791b1ec8
Brent Vollebregt <brent.vollebregt@gmail.com>
2020-06-15 17:35:11 +1200

    Fixed portfolio layout on smaller screens

--- src/cms/preview-templates/PortfolioPagePreview.tsx
+++ src/cms/preview-templates/PortfolioPagePreview.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { PortfolioPageTemplate } from "../../templates/portfolio-page";
 
 const PortfolioPagePreview = ({ widgetFor }) => {
-  return <PortfolioPageTemplate snippets={() => widgetFor("body")} />;
+  return (
+    <div className="mx-1">
+      <PortfolioPageTemplate snippets={() => widgetFor("body")} />
+    </div>
+  );
 };
 
 export default PortfolioPagePreview;
--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -12,8 +12,8 @@ const Portfolio: React.FC<IPortfolio> = ({ snippets }) => {
   const Snippets = snippets;
 
   return (
-    <div className="portfolio row justify-content-center">
-      <div className="col-xs-12 col-lg-8">
+    <div className="portfolio">
+      <div className="col-xs-12 col-lg-8 d-block mx-auto">
         <h1 className="text-center">Portfolio</h1>
         <p className="lead text-center">
           This is a small collection of my favourite projects I developed with a small description
@@ -21,7 +21,7 @@ const Portfolio: React.FC<IPortfolio> = ({ snippets }) => {
         </p>
       </div>
 
-      <div className="row masonry" ref={highlightRoot}>
+      <div className="d-flex mw-100 masonry" ref={highlightRoot}>
         <Snippets />
       </div>
     </div>
