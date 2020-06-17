64379b9cc3832f022cce41503b92e67705e7dc61
Brent Vollebregt <brent.vollebregt@gmail.com>
2020-06-17 18:10:59 +1200

    Try to fix blur effect not disappearing on transparent images

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,8 @@ module.exports = {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 2048
+              maxWidth: 2048,
+              disableBgImageOnAlpha: false
             }
           },
           {
