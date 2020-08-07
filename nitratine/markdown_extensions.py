3e50405798d0a9f3a352070c2da84eb56df63122
Brent Vollebregt <brent.vollebregt@gmail.com>
2020-08-08 09:49:36 +1200

    Add anchor links to headers

--- nitratine/markdown_extensions.py
+++ nitratine/markdown_extensions.py
@@ -1,5 +1,6 @@
 from markdown.extensions import Extension
 from markdown.inlinepatterns import Pattern
+from markdown.treeprocessors import Treeprocessor
 
 from xml.etree.ElementTree import Element, SubElement
 
@@ -39,3 +40,37 @@ class YouTubeVideoExtension(Extension):
 
     def extendMarkdown(self, md, md_globals):
         md.inlinePatterns['youtube-video'] = YouTubeVideoPattern(self.getConfigs(), md)
+
+
+class HeaderLinkProcessor(Treeprocessor):
+    SUPPORTED_ELEMENTS = ['h2', 'h3', 'h4', 'h5', 'h6']
+    SVG_ICON_PATH = 'M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z'
+
+    def run(self, root):
+        for element in root:
+            if element.tag in self.SUPPORTED_ELEMENTS:
+                # Add a wrapper so we can keep using :target::before on the header
+                span_wrapper = SubElement(element, 'span')
+                span_wrapper.set('style', 'position:relative')
+                # Setup the new `a` tag
+                link_element = SubElement(span_wrapper, 'a')
+                link_element.tail = element.text  # Put text after the a tag
+                element.text = '\n'  # Remove the text from the original header
+                link_element.set('class', 'anchor')
+                link_element.set('href', f'#{element.get("id")}')
+                # Setup the internal SVG
+                svg_element = SubElement(link_element, 'svg')
+                svg_element.set('aria-hidden', 'true')
+                svg_element.set('focusable', 'false')
+                svg_element.set('height', '16')
+                svg_element.set('width', '16')
+                svg_element.set('version', '1.1')
+                svg_element.set('viewBox', '0 0 16 16')
+                path_element = SubElement(svg_element, 'path')
+                path_element.set('fill-rule', 'evenodd')
+                path_element.set('d', self.SVG_ICON_PATH)
+
+
+class HeaderLinkExtension(Extension):
+    def extendMarkdown(self, md):
+        md.treeprocessors.register(HeaderLinkProcessor(md), 'headerlinkprocessor', 1)
