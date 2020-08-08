7bb2b71c9b089675f675af73be7f05a9a18a77a4
Brent Vollebregt <brent.vollebregt@gmail.com>
2020-08-08 17:41:52 +1200

    Add hash reference tests

--- tests/test_links_exist.py
+++ tests/test_links_exist.py
@@ -17,12 +17,15 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 
 
 def get_urls_from_html(html: str) -> List[str]:
+    """ Identify all the links from HTML """
     links = []
     soup = BeautifulSoup(html, 'html.parser')
 
     for link in soup.findAll('a'):
         if link.get('href') is not None:
             links.append(link.get('href'))
+
+    # TODO Add script sources
 
     return list(set(links))
 
@@ -62,7 +65,7 @@ class TestLinksRespondNon404(unittest.TestCase):
 
             # Filter out some potential values we don't want
             if link.startswith('#') or link.startswith('mailto:') or link.startswith('javascript:'):
-                continue  # TODO Check hash is on page (different test)
+                continue
 
             # Validate external link is not in blacklist
             if any([re.match(r, link) is not None for r in EXTERNAL_URL_BLACKLIST]):
@@ -75,6 +78,45 @@ class TestLinksRespondNon404(unittest.TestCase):
                 headers={'User-Agent': USER_AGENT}
             )
             self.assertEqual(response.status_code, 200, f'The path "{link}" returned HTTP{response.status_code}')
+
+    def test_hash_references(self):
+        test_client = app.test_client(self)
+        pages = {}  # Store each page requested locally as a BeautifulSoup object
+        links_visited = []
+        links_to_visit = ['/']
+        links_with_hashes_to_check = []
+
+        # Initially only search local routes to build up a list of local links with hashes
+        while len(links_to_visit) != 0:
+            link = links_to_visit.pop()
+            links_visited.append(link)
+
+            # Get the page
+            response = test_client.get(link)
+
+            if 'text/html' in response.content_type:
+                html = response.data.decode()
+                pages[link] = BeautifulSoup(html, 'html.parser')  # Save as BeautifulSoup so we don't have to do this more than once per page
+                urls_within = get_urls_from_html(html)
+                links_to_visit += [
+                    u for u in urls_within
+                    if u.startswith('/')
+                       and u.endswith('/')  # Do not look for hashes here
+                       and u not in links_visited
+                       and u not in links_to_visit
+                ]
+                # Links with hashes can either start with just a hash (for this page) or have a hash in them
+                links_with_hashes_to_check += [link + u for u in urls_within if u.startswith('#')] \
+                                + [u for u in urls_within if u.startswith('/') and '#' in u]  # Start with / for this site
+
+        links_with_hashes_to_check = list(set(links_with_hashes_to_check))  # Distinct list
+        while len(links_with_hashes_to_check) != 0:
+            link = links_with_hashes_to_check.pop()
+            self.assertEqual(1, link.count('#'), f'"{link}" has more than one "#"')
+            # Identify the id and look for it on the page it is referencing
+            route, id = link.split('#')
+            soup = pages[route]
+            self.assertNotEqual(None, soup.find(id=id))
 
 
 if __name__ == '__main__':
