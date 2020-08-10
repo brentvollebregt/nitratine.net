6e7ba47bce4105b371b29e3d01242fb3e82db13b
Brent Vollebregt <brent.vollebregt@gmail.com>
2020-08-10 20:27:34 +1200

    Add livereload to run

--- nitratine/__main__.py
+++ nitratine/__main__.py
@@ -1,10 +1,14 @@
+from pathlib import Path
+
 import click
+from livereload import Server
 
 from .site import app, setup_minification
 from .build import build as build_site
 from .tools.new_post import new_post
 from .tools.serve_build import serve_build
 from .tools.build_stats import print_build_stats
+from .config import POST_SOURCE
 
 
 DEFAULT_HOST = 'localhost'
@@ -20,13 +24,20 @@ def cli():
 @click.option('-h', '--host', 'host', default=DEFAULT_HOST, help="Hostname to run server with")
 @click.option('-p', '--port', 'port', type=int, default=DEFAULT_PORT, help="Port to run server on")
 @click.option('--minify', 'minify', is_flag=True, default=False, help="Enable minification")
-def run(host: str, port: int, minify: bool):
+@click.option('--watch', 'watch', is_flag=True, default=False, help="Enable live-reload and watch for changes")
+def run(host: str, port: int, minify: bool, watch: bool):
     """ Run the development site locally """
     if minify:
         setup_minification()
 
-    print(f'Server starting at http://{host}:{port}')
-    app.run(host=host, port=port)
+    if watch:
+        server = Server(app)
+        server.watch(POST_SOURCE)
+        server.watch(str(Path(__file__).resolve().parent / 'static'))
+        server.serve(host=host, port=port)
+    else:
+        print(f'Server starting at http://{host}:{port}')
+        app.run(host=host, port=port)
 
 
 @cli.command()
