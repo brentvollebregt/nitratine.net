208706b0dfe789829f1730417810a9fc24dfa1f0
brentvollebregt <brentvollebregt@users.noreply.github.com>
2024-11-16 05:39:31 +0000

    deploy: 67cbb21cfd52765a8241dfc489fedc09e6808cc7

---
+++ static/js/search.js
@@ -0,0 +1,27 @@
+function doSearch(inputElement) {
+  window.location.href =
+    searchUrl + "?q=" + encodeURIComponent(inputElement.value);
+}
+
+const sidebarSearchInput = document.getElementById("sidebar-search");
+const sidebarSearchButton = document.getElementById("sidebar-search-submit");
+const searchPageSearchInput = document.getElementById("search");
+
+sidebarSearchButton.addEventListener("click", function () {
+  doSearch(sidebarSearchInput);
+});
+sidebarSearchInput.addEventListener("keyup", function (e) {
+  e.preventDefault();
+  if ((e.keyCode ? e.keyCode : e.which) === 13) {
+    doSearch(sidebarSearchInput);
+  }
+});
+
+if (searchPageSearchInput !== null) {
+  searchPageSearchInput.addEventListener("keyup", function (e) {
+    e.preventDefault();
+    if ((e.keyCode ? e.keyCode : e.which) === 13) {
+      doSearch(searchPageSearchInput);
+    }
+  });
+}
