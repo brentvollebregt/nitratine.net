75a48a69754fbf635141a19352e1bb5d97c1bd7d
Brent Vollebregt <brent.vollebregt@gmail.com>
2025-10-04 17:13:16 +1300

    Add exclusion to link test

--- tests/test_links_exist.py
+++ tests/test_links_exist.py
@@ -29,7 +29,8 @@ EXTERNAL_URL_BLACKLIST = [
     r'^https://unsplash.com',  # 403
     r'^https://github.com',  # 429
     r'^https://www.cvedetails.com',  # 403
-    r'^https://fotoforensics.com'  # 403
+    r'^https://fotoforensics.com',  # 403
+    r'^https://www.digitalcitizen.life'  # SSL error
 ]
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
 
