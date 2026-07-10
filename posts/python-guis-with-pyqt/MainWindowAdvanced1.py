4b529a40e4f6428bee156143245e35c17dcf8fb8
brentvollebregt <brentvollebregt@users.noreply.github.com>
2026-07-10 08:42:58 +0000

    deploy: d1e5e95c7c1ad1f49ac43bcee77159ef46859b59

---
+++ posts/python-guis-with-pyqt/MainWindowAdvanced1.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\Brent\Desktop\MainWindowAdvanced.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(481, 360)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.selectImageBtn = QtWidgets.QPushButton(self.centralwidget)
+        self.selectImageBtn.setGeometry(QtCore.QRect(10, 40, 101, 71))
+        font = QtGui.QFont()
+        font.setFamily("Century Gothic")
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setWeight(50)
+        self.selectImageBtn.setFont(font)
+        self.selectImageBtn.setObjectName("selectImageBtn")
+        self.imageLbl = QtWidgets.QLabel(self.centralwidget)
+        self.imageLbl.setGeometry(QtCore.QRect(120, 10, 351, 181))
+        self.imageLbl.setFrameShape(QtWidgets.QFrame.Box)
+        self.imageLbl.setText("")
+        self.imageLbl.setObjectName("imageLbl")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(10, 230, 161, 20))
+        self.lineEdit.setObjectName("lineEdit")
+        self.addBtn = QtWidgets.QPushButton(self.centralwidget)
+        self.addBtn.setGeometry(QtCore.QRect(10, 250, 161, 23))
+        self.addBtn.setObjectName("addBtn")
+        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget.setGeometry(QtCore.QRect(180, 200, 291, 111))
+        self.listWidget.setObjectName("listWidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 481, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.selectImageBtn.setText(_translate("MainWindow", "Select Image"))
+        self.addBtn.setText(_translate("MainWindow", "Add"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
